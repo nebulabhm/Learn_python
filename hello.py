85cbea37c761376c314397acbfa21abdf3cb66ce
nebulabhm <nebulabhm@gmail.com>
2024-09-25 22:05:46 +0800

    hello world of python

---
+++ hello.py
@@ -0,0 +1,11 @@
+'''
+Author: nebulabhm nebulabhm@gmail.com
+Date: 2024-09-25 21:40:12
+LastEditors: nebulabhm nebulabhm@gmail.com
+LastEditTime: 2024-09-25 21:40:28
+FilePath: \program\Learn_python\hello.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+name = input('please enter your name:')
+print('hello,world!',name)
+
