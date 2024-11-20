87da321390ab3841ec76a9085c9d0ef2845beb8d
nebulabhm <nebulabhm@gmail.com>
2024-11-20 20:43:43 +0800

    /*  * @Author: nebulabhm nebulabhm@gmail.com  * @Date: 2024-09-25 22:05:46  * @LastEditors: nebulabhm nebulabhm@gmail.com  * @LastEditTime: 2024-11-20 20:44:25  * @FilePath: \Learn_python\.git\COMMIT_EDITMSG  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE  */

---
+++ myCats.py
@@ -0,0 +1,10 @@
+'''
+Author: nebulabhm nebulabhm@gmail.com
+Date: 2024-11-17 22:04:53
+LastEditors: nebulabhm nebulabhm@gmail.com
+LastEditTime: 2024-11-17 22:07:57
+FilePath: \Learn_python\myCats.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+cats = [{'desc': 'cubby', 'name': 'Zophie'}, {'desc': 'fluffy', 'name': 'Pooka'}]
+    
